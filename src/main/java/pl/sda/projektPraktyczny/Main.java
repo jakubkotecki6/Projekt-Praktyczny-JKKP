2084a2f92c652336b53783141c267fd7ac9e33a3
Jakub Kotecki <jakubkotecki6@gmail.com>
2023-07-17 18:02:45 +0200

    BRANCH TEST

--- src/main/java/pl/sda/projektPraktyczny/Main.java
+++ src/main/java/pl/sda/projektPraktyczny/Main.java
@@ -3,5 +3,9 @@ package pl.sda.projektPraktyczny;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //TEST
+        //TEST
+        //TEST
+        //TEST
     }
 }
