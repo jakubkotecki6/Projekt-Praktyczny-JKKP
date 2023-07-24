5812f0efece83110d89b5c9f3c3fbdf2a2df9cd7
jakubkotecki6 <jakubkotecki6@gmail.com>
2023-07-24 20:55:02 +0200

    DB

--- src/main/java/pl/sda/projektPraktyczny/services/ProductService.java
+++ src/main/java/pl/sda/projektPraktyczny/services/ProductService.java
@@ -1,14 +1,39 @@
 package pl.sda.projektPraktyczny.services;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import pl.sda.projektPraktyczny.models.Category;
 import pl.sda.projektPraktyczny.models.Product;
 
+import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public final class ProductService {
 
-    private final List<Product> products = new ArrayList<>();
+    private static List<Product> products = new ArrayList<>();
+
+    public static void generateProductsList() {
+        Gson gson = new GsonBuilder().enableComplexMapKeySerialization().setPrettyPrinting().create();
+        try (Writer writer = new FileWriter("ProductsDB.JSON")) {
+            gson.toJson(products, writer);
+        } catch (IOException e) {
+            System.err.println("Wystąpił błąd podczas zapisywania listy zamówień " + e.getMessage());
+        }
+    }
+
+    public void loadProductsFromFile() {
+        try (Reader reader = new FileReader("ProductsDB.JSON")) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            Type type = new TypeToken<ArrayList<Product>>(){}.getType();
+            products = gson.fromJson(reader, type);
+        } catch (IOException e) {
+            System.err.println("Wystąpił błąd podczas odczytywania listy zamówień " + e.getMessage());
+        }
+    }
 
     public List<Product> getProducts() {
         return products;
